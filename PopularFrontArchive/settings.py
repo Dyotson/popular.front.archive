75ea5b7d70ea31fbffb062a249df18f7d0a2a3d8
Dyotson <mjmilitzer@uc.cl>
2023-10-12 20:01:41 -0300

    Changed database to postgresql

--- PopularFrontArchive/settings.py
+++ PopularFrontArchive/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 from dotenv import load_dotenv
 import os
+import dj_database_url
 
 load_dotenv()
 
@@ -81,10 +82,10 @@ WSGI_APPLICATION = "PopularFrontArchive.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default="postgresql://postgres:postgres@localhost:5432/popularfrontarchive",
+        conn_max_age=600,
+    )
 }
 
 
